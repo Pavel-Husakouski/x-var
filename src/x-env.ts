3bfa78a422b948175a5f96cfe4e7c66a3bf474a1
Pauel Husakouski <phusakouski@gmail.com>
2024-02-08 21:45:57 +0300

    add prompt

--- src/x-env.ts
+++ src/x-env.ts
@@ -11,6 +11,12 @@ import { extractArgs } from './parse';
 function main() {
     const [cmdLine, path, env] = extractArgs(process.argv.slice(2));
 
+    if(cmdLine.length === 0) {
+        console.error('Usage: x-env [-e <path to .env file>] [[KEY=VALUE] <list of variables>] <command> [args...]');
+
+        exit(1);
+    }
+
     config({ path: path || '.env' });
 
     const [command, ...args] = interpolate(cmdLine, { ...process.env, ...env });
--- src/x-var.ts
+++ src/x-var.ts
@@ -4,10 +4,18 @@ import * as spawn from 'cross-spawn';
 // @ts-ignore
 import * as exit from 'exit';
 import { interpolate } from './interpolate';
+import { extractArgs } from './parse';
 
 function main() {
-    const cmdLine = process.argv.slice(2);
-    const [command, ...args] = interpolate(cmdLine, process.env);
+    const [cmdLine, path, env] = extractArgs(process.argv.slice(2));
+
+    if(cmdLine.length === 0) {
+        console.error('Usage: x-var [[KEY=VALUE] <list of variables>] <command> [args...]');
+
+        exit(1);
+    }
+
+    const [command, ...args] = interpolate(cmdLine, {...process.env, ...env});
     const proc = spawn.sync(command, args, { stdio: 'inherit' });
 
     exit(proc.status);
