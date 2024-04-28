4d65c626d8b626541bda51ac963d9912a6105d5b
Pavel Husakouski <phusakouski@gmail.com>
2024-04-28 14:25:09 +0300

    drop exit package

--- src/x-env.ts
+++ src/x-env.ts
@@ -1,8 +1,6 @@
 #!/usr/bin/env node
 
 import * as spawn from 'cross-spawn';
-// @ts-ignore
-import * as exit from 'exit';
 import { config } from 'dotenv';
 import * as process from 'process';
 import { interpolate } from './interpolate';
@@ -14,7 +12,7 @@ function main() {
     if(cmdLine.length === 0) {
         console.error('Usage: x-env [-e <path to .env file>] [[KEY=VALUE] <list of variables>] <command> [args...]');
 
-        exit(1);
+        process.exit(1);
     }
 
     config({ path: path || '.env' });
@@ -22,7 +20,7 @@ function main() {
     const [command, ...args] = interpolate(cmdLine, { ...process.env, ...env });
     const proc = spawn.sync(command, args, { stdio: 'inherit' });
 
-    exit(proc.status);
+    process.exit(proc.status);
 }
 
 main();
--- src/x-var.ts
+++ src/x-var.ts
@@ -1,8 +1,6 @@
 #!/usr/bin/env node
 
 import * as spawn from 'cross-spawn';
-// @ts-ignore
-import * as exit from 'exit';
 import { interpolate } from './interpolate';
 import { extractArgs } from './parse';
 
@@ -12,13 +10,13 @@ function main() {
     if(cmdLine.length === 0) {
         console.error('Usage: x-var [[KEY=VALUE] <list of variables>] <command> [args...]');
 
-        exit(1);
+        process.exit(1);
     }
 
     const [command, ...args] = interpolate(cmdLine, {...process.env, ...env});
     const proc = spawn.sync(command, args, { stdio: 'inherit' });
 
-    exit(proc.status);
+    process.exit(proc.status);
 }
 
 main();
